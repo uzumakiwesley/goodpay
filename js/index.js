bc18e07e51fda48266fac8238f1855448534ec37
uzumakiwesley <uzumakiwesley58@gmail.com>
2022-09-30 13:03:18 -0300

    commit_goodpay_3009

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/**
+ * trouxe o array do arquivo moduloDados.js para o index.js
+ */
+
+import usuarios from "./moduloDados.js"
+
+console.log(usuarios)
---
+++ js/moduloDados.js
@@ -0,0 +1,53 @@
+/**
+ * Dentro desse Array existem usuários com esta estrutura:
+ * - id
+ * - nomeUsuario
+ * - senha
+ * - email
+ */
+
+const usuarios = [ 
+    {
+    id:  1,
+    nomeUsuario: "wesley-ribeiro",
+    senha: "12345",
+    email: "uzumakiwesley58@gmail.com",
+    } ,
+
+    {
+        id:  2,
+        nomeUsuario: "mario-armario",
+        senha: "12345",
+        email: "mario.armario@gmail.com",
+        } ,
+
+        {
+            id:  3,
+            nomeUsuario: "jose-roberto",
+            senha: "12345",
+            email: "jose.roberto@gmail.com",
+            } ,
+        
+            {
+                id:  4,
+                nomeUsuario: "maico-caiqui",
+                senha: "12345",
+                email: "maico.caiqui@gmail.com",
+                } ,
+
+                {
+                    id:  5,
+                    nomeUsuario: "atila-ruiz",
+                    senha: "12345",
+                    email: "atila.ruiz@gmail.com",
+                    } ,
+                
+]
+
+export default usuarios;
+
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ */
+
+
