5e1aea511f3f0b8b32cd57395cc095651f3c1f70
uzumakiwesley <uzumakiwesley58@gmail.com>
2022-10-03 17:16:02 -0300

    goodpay_0310

--- js/index.js
+++ js/index.js
@@ -4,4 +4,26 @@
 
 import usuarios from "./moduloDados.js"
 
-console.log(usuarios)
+const btnEntrar = document.getElementById('btnEntrar')
+const alertaLogin = new bootstrap.Modal('#alertaLogin')
+
+btnEntrar.addEventListener('click', () => {
+    const inputLogin = document.getElementById('login').value
+    const inputSenha = document.getElementById('senha').value
+
+    let usuarioValido;
+
+    for(let usuario of usuarios) {
+        if(usuario.nomeUsuario == inputLogin) {
+            usuarioValido = usuario;
+            break
+        }
+
+    }
+
+        if(inputSenha == usuarioValido.senha) {
+            window.location.pathname = '/app.html'
+        } else {
+            alertaLogin.show()
+        }
+})
